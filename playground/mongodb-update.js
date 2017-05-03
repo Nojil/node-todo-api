70dffd5b926ef24f7e14fa828b0dc3e9b652acc3
Charles <clang676@gmail.com>
2017-05-02 20:10:47 -0600

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server on port 27017');
+
+    //findOneAndUpdate
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("59093285ba97c18bd278fc7f")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOrigional: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("590014e2b57eb206ba805261")
+    }, {
+        $set: {
+            name: 'Charles'
+        },
+        $inc: {
+            age: +1
+        }
+    }, {
+        returnOrigional: false
+    }).then((result) => {
+        console.log(result);
+    })
+
+    //db.close()
+});
