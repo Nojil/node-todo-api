c09531fecf7182220ef88a699ee0dda82b95ccaa
Charles <clang676@gmail.com>
2017-05-04 14:42:49 -0600

    Refactored code, added POST api route

--- server/server.js
+++ server/server.js
@@ -1,40 +1,27 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
+var app = express();
 
-// const newTodo = new Todo({
-//     text: 'Cook Dinner'
-// });
-//
-// newTodo.save()
-//     .then((doc) => {
-//         console.log('Saved todo', doc);
-//     }, (e) => {
-//         console.log('Unable to save todo');
-//     });
+app.use(bodyParser.json());
 
-const newTodo2 = new Todo({
-    text: 'Eat Dinner',
-    completed: true,
-    completedAt: 5
-});
-
-newTodo2.save()
-    .then((doc) => {
-        console.log(JSON.stringify(doc, undefined, 2 ));
-    }, (e) => {
-        console.log('Unable to save todo');
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
     });
+
+    todo.save()
+        .then((doc) => {
+            res.send(doc);
+        }, (e) => {
+            res.status(400).send(e);
+        });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
