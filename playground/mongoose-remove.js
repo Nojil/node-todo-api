df1cbfc6628e6cc3c36818301798d1db52547fcf
Charles <clang676@gmail.com>
2017-05-19 22:12:17 -0600

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,23 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} =  require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+// Todo.remove({}).
+//     then((result) => {
+//         console.log(result);
+//     });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findOneAndRemove({_id: '591fbad30af332b7690d0b03'})
+    .then((todo) => {
+
+    });
+
+// Todo.findByIdAndRemove('591fbad30af332b7690d0b03')
+//     .then((todo) => {
+//         console.log(todo);
+//     });
--- server/server.js
+++ server/server.js
@@ -50,7 +50,26 @@ app.get('/todos/:id', (req, res) => {
         }).catch((e) => {
             res.status(400).send();
         });
-})
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then((todos) => {
+            if (!todos) {
+                return res.status(404).send();
+            }
+
+            res.send({todos});
+        }).catch((e) => {
+            res.status(400).send();
+        });
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
